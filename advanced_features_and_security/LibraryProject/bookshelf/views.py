1451ac5226214b3d4c99934c222fa82024b46be7
davembalo <dsmbalo@gmail.com>
2024-11-24 22:50:40 +0200

    Managing permissions and groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,3 +4,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request): return HttpResponse("Welcome to the home page.")
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+@permission_required('bookshelf.can_add_book', raise_exception=True)
+def create_book(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        publication_year = request.POST.get('publication_year')
+        Book.objects.create(title=title, author=author, publication_year=publication_year)
+        return redirect('book_list') 
+    return render(request, 'create_book.html')
+
+@permission_required('bookshelf.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+        return redirect('book_detail', book_id=book.id)  
+    return render(request, 'edit_book.html', {'book': book})
+
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect('book_list')  
+    return render(request, 'delete_book.html', {'book': book})
+
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'view_book.html', {'book': book})
